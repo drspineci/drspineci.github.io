b30af718760a63dc282ef20069e1aaf9c4233e96
root <spineci@consultant.com>
2022-06-06 00:52:32 +0000

    meidei

---
+++ mygit.sh
@@ -0,0 +1,1 @@
+git clone https://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@github.com/drspineci/drspineci.github.io
---
+++ they-reup.sh
@@ -0,0 +1,1 @@
+git push https://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@github.com/drspineci/drspineci.github.io
